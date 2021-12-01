c832154477d63d143ec5d85d20d01934c247183d
SergeySmolyakov <93509426+SergeySmolyakov@users.noreply.github.com>
2021-12-01 15:28:21 +0300

    Add files via upload

---
+++ homework2JS/4.js
@@ -0,0 +1,16 @@
+function addition(a,b) { 
+    return (a + b);    
+}
+function subtraction(a,b) { 
+    return (a - b);
+}
+function division(a,b) { 
+    return (a / b);
+}
+function multiplication(a,b) { 
+    return (a * b);
+}
+console.log(addition(5,6));
+console.log(subtraction(5,6));
+console.log(division(5,6));
+console.log(multiplication(5,6));
